7744a6c5372596dbd0e58394b56841c7fe2c2705
Yifeng Hou <yhou@pivotal.io>
2017-11-10 11:01:22 +0800

    Initial Commmit
    Signed-off-by: Yaofeng Wang <wangyf@globalpsa.com>

---
+++ src/test/java/psa/trial/TrialApplicationTests.java
@@ -0,0 +1,16 @@
+package psa.trial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
