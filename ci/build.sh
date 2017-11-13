6220bb97dfd357df95a6af9620482b660f3ec73b
Yaofeng Wang <wangyf@globalpsa.com>
2017-11-13 11:09:41 +0800

    Change output folder
    Signed-off-by: Yifeng Hou <yhou@pivotal.io>

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,6 @@ set -e -u -x
 cd trial-psa/
 ./gradlew build
 
-cp trial-psa/build/libs/trial-0.0.1-SNAPSHOT.jar build-src/.
+cp build/libs/trial-0.0.1-SNAPSHOT.jar ../build-src/.
 
-ls build-src
+ls ../build-src
